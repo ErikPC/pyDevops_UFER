f6f605d5e76593d6fa980b61e07e428f67aca3b5
ErikPC <epardillo@cifpfbmoll.eu>
2021-12-03 15:48:06 +0100

    :recycle: [data_deletion] cambiar nombre de input

--- src/model/data_deletion/data_deletion.py
+++ src/model/data_deletion/data_deletion.py
@@ -12,13 +12,12 @@ def enseñar_items(database):
 #         selector = False
 
 
-def delete_data(database):
-    documentos = database.find({"services": {"$exists": True}})
+def delete_data(collection):
+    documentos = collection.find({"services": {"$exists": True}})
     servicios = []
     for item in documentos:
         print(item)
         servicios.append(item["services"])
-    # items (funcion)
     servicio_eliminar = input("Escribe el servicio que quieres eliminar: \n")
     confirm = input("Vuelve a escribir el servicio: \n")
     if servicio_eliminar != confirm:
