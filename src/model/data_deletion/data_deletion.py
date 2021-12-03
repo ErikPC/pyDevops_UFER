260440a1066f5fc22516c4fa886e7ed7321a748c
ErikPC <epardillo@cifpfbmoll.eu>
2021-12-03 15:09:49 +0100

    :white_check_mark:[data_deletion] pasar casos test

--- src/model/data_deletion/data_deletion.py
+++ src/model/data_deletion/data_deletion.py
@@ -1,35 +1,31 @@
-from pymongo import MongoClient
-from src.repository.db_connection.connect_db import connect_db
-import pymongo
-database = connect_db()
-collection = database['pruebas']
-# Cuenado esto funcione , hay que cambiar 'pruebas' por el nombre de de la coleccion definitiva
-collection.insert_one(
-    {
-        'nombre': 'Joan',
-        'edad': 20,
-        'deportes': ['padel', 'futbol', 'boxeo']
-    }
-)
 
 
 def enseñar_items(database):
     #     keys = ['nombre', 'edad', 'deportes']
     for item in collection.find({}, {'_id': False}):
-        print(item)
-
-
+        pass
 #     while selector == True:
 #         key_entrada = input("Escribe la key quieres filtrar / si no quieres escribe fin")
 #         if key_entrada in keys:
 #             collection.find({key_entrada})
 #             break
 #         selector = False
-items = enseñar_items(database)
 
 
 def delete_data(database):
+    documentos = database.find({"services": {"$exists": True}})
+    servicios = []
+    for item in documentos:
+        print(item)
+        servicios.append(item["services"])
     # items (funcion)
-    # funcion de seleccionar servicio
-    # comparar que el input esta bien escritos
-    # eliminar item por servicio
+    servicio_eliminar = input("Escribe el servicio que quieres eliminar: \n")
+    confirm = input("Vuelve a escribir el servicio: \n")
+    if servicio_eliminar != confirm:
+        print("Los servicios no coinciden")
+    elif servicio_eliminar in servicios:
+        database.delete_many({"services": servicio_eliminar})
+        print("Se ha eliminado correctamente")
+    else:
+        print("item no encontrado")
+
