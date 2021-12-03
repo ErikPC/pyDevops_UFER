7d68ce8baa0fc39b15370dae1d951c7bfe5ffdb4
Joan Nicolau <jnicolausantandreu@cifpfbmoll.eu>
2021-12-03 20:08:03 +0100

    :art: [test] mejorar formato del codigo segun PEP 8

--- test/test_validar_schema_keys.py
+++ test/test_validar_schema_keys.py
@@ -1,8 +1,10 @@
 import pytest
 from src.model.data_validation.validate_schema_keys import validate_keys
+
 ufer_schema_invalid_keys = {
     "nombre": "Ufer Gold",
-    "description": "Ufer Gold offers the most luxury experience in the entire galaxy. Includes all the features and amenities you can imagine.",
+    "description": "Ufer Gold offers the most luxury experience in the entire galaxy. Includes all the features and "
+                   "amenities you can imagine.",
     "conductor": "Autopilot",
     "passengers": 1,
     "privacy": "Private",
@@ -10,11 +12,13 @@ ufer_schema_invalid_keys = {
     "propulsion": "Photon Engine",
     "top_speed": 1080000000,
     "price": 120,
-    "amenities": ["WiFi", "Bluetooth", "Tinted Screens", "HiFi Audio System", "A/C", "Champagne Bottle", "Water", "Snacks"]
+    "amenities": ["WiFi", "Bluetooth", "Tinted Screens", "HiFi Audio System", "A/C", "Champagne Bottle", "Water",
+                  "Snacks"]
 }
 ufer_schema_invalid_keys_and_types = {
     "nombre": "Ufer Gold",
-    "description": "Ufer Gold offers the most luxury experience in the entire galaxy. Includes all the features and amenities you can imagine.",
+    "description": "Ufer Gold offers the most luxury experience in the entire galaxy. Includes all the features and "
+                   "amenities you can imagine.",
     "conductor": "Autopilot",
     "passengers": 1,
     "privacy": "Private",
@@ -22,11 +26,13 @@ ufer_schema_invalid_keys_and_types = {
     "propulsion": "Photon Engine",
     "top_speed": 1080000000,
     "price": "120",
-    "amenities": ["WiFi", "Bluetooth", "Tinted Screens", "HiFi Audio System", "A/C", "Champagne Bottle", "Water", "Snacks"]
+    "amenities": ["WiFi", "Bluetooth", "Tinted Screens", "HiFi Audio System", "A/C", "Champagne Bottle", "Water",
+                  "Snacks"]
 }
 ufer_schema_valid = {
     "name": "Ufer Gold",
-    "description": "Ufer Gold offers the most luxury experience in the entire galaxy. Includes all the features and amenities you can imagine.",
+    "description": "Ufer Gold offers the most luxury experience in the entire galaxy. Includes all the features and "
+                   "amenities you can imagine.",
     "driver": "Autopilot",
     "passengers": 1,
     "privacy": "Private",
@@ -34,12 +40,16 @@ ufer_schema_valid = {
     "propulsion": "Photon Engine",
     "top_speed": 1080000000,
     "price": 120,
-    "amenities": ["WiFi", "Bluetooth", "Tinted Screens", "HiFi Audio System", "A/C", "Champagne Bottle", "Water", "Snacks"]
+    "amenities": ["WiFi", "Bluetooth", "Tinted Screens", "HiFi Audio System", "A/C", "Champagne Bottle", "Water",
+                  "Snacks"]
 }
+
+
 @pytest.mark.test_validar_keys
 def test_validar_keys():
     assert validate_keys(ufer_schema_invalid_keys) == False
     assert validate_keys(ufer_schema_invalid_keys_and_types) == False
+
 
 @pytest.mark.test_validar_keys
 def test_validar_keys():
