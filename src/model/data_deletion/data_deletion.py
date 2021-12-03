40045e305aa2142a1f75f77e1ec69dea9d950909
ErikPC <epardillo@cifpfboll.eu>
2021-12-03 12:09:59 +0100

    :art:[data_deletion] hacer pruebas

--- src/model/data_deletion/data_deletion.py
+++ src/model/data_deletion/data_deletion.py
@@ -1,9 +1,25 @@
-from pymongo import collection, database
+from pymongo import MongoClient
 from src.repository.db_connection.connect_db import connect_db
 import pymongo
 database = connect_db()
-collection = database[]
+collection = database['pruebas']
+# Cuenado esto funcione , hay que cambiar 'pruebas' por el nombre de de la coleccion definitiva
+collection.insert_one(
+    {
+        'nombre': 'Joan',
+        'edad': 20,
+        'deportes': ['padel', 'futbol', 'boxeo']
+    }
+)
 
-print(view_db)
-def eliminar_data(database):
-    pass
+
+def enseñar_items(database):
+    keys = ['nombre', 'edad', 'deportes']
+    for item in collection.find({}):
+        print(item)
+    while selector == True:
+        key_entrada = input("Escribe la key quieres filtrar / si no quieres escribe fin")
+        if key_entrada in keys:
+            collection.find({key_entrada})
+            break
+        selector = False
