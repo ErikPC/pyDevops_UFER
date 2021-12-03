9cafedf2779c266a2cfaf882c9268930a48de1c1
ErikPC <epardillo@cifpfbmoll.eu>
2021-12-03 11:20:09 +0100

    :hammer: [data_deletion] crear modulo

---
+++ src/model/data_deletion/data_deletion.py
@@ -0,0 +1,9 @@
+from pymongo import collection, database
+from src.repository.db_connection.connect_db import connect_db
+import pymongo
+database = connect_db()
+collection = database[]
+
+print(view_db)
+def eliminar_data(database):
+    pass
