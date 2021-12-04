84570ea637359c7a7fbed0de1499fc12d4e621b2
Joan Nicolau <jnicolausantandreu@cifpfbmoll.eu>
2021-12-04 13:40:21 +0100

    :white_check_mark: [test] escribir y pasar tests validar schema

--- src/model/data_creation/create_service/create_service.py
+++ src/model/data_creation/create_service/create_service.py
@@ -1,13 +1,25 @@
 from src.model.data_validation import validate_schema
 
+
 def create_service(collection):
-    print("Para crear un servicio, sigue el siguiente schema:\n" "{\n \t\"name\": \"\",\n\t\"description\": \"\",\n\t \"driver\": \"\",\n\t\"passengers\": \"\",\n\t \"privacy\": \"\",\n\t \"seats\": \"\",\n\t \"propulsion\": \"\",\n\t \"top_speed\": 0,\n\t \"amenities\": []\n\t }")
+    item_template = {
+        "name": "",
+        "description": "",
+        "driver": "",
+        "passengers": "",
+        "privacy": "",
+        "seats": "",
+        "propulsion": "",
+        "top_speed": 0,
+        "amenities": []
+    }
 
-    servicio_creado = input("Porfavor siga el schema:\n")
+    user_instructions = "Para crear un servicio, sigue el siguiente schema:\n" "{\n \t\"name\": \"\"," \
+                        "\n\t\"description\": \"\",\n\t " "\"driver\": \"\",\n\t\"passengers\": \"\"," \
+                        "\n\t \"privacy\": \"\",\n\t \"seats\": \"\",\n\t \"propulsion\": ""\"\",\n\t \"top_speed\": " \
+                        "0,\n\t \"amenities\": []\n\t } "
 
-    is_valid = validate_schema(servicio_creado)
+    # tell the user how to fill the values
+    print(user_instructions)
 
-    if is_valid == True:
-        collection.insert_one(servicio_creado)
-    else:
-        print("no es un schema valido")
+
