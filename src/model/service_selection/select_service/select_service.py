6aaeb587530c26c93d9b38c9fc9e6b78ea230729
Joan Nicolau <jnicolausantandreu@cifpfbmoll.eu>
2021-12-04 15:47:51 +0100

    :recycle: [select service] mejorar funcion seleccionar servicio

--- src/model/service_selection/select_service/select_service.py
+++ src/model/service_selection/select_service/select_service.py
@@ -1,8 +1,10 @@
 def select_service(services):
     while True:
-        service = input("enter an existing service: ").strip()
+        service = input("enter an existing service: ").strip().title()
         if service in services:
             break
     print('the selected service is', service)
     return service
 
+
+select_service(['Ufer Gold', 'Ufer Lite', 'Ufer Bus'])
