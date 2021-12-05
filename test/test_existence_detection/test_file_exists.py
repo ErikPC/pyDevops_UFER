4f1098e4956ad97d9727390319525bff12c62675
Joan Nicolau <jnicolausantandreu@cifpfbmoll.eu>
2021-12-05 12:27:41 +0100

    :white_check_mark: [test] pasar tests de file_exists

--- test/test_existence_detection/test_file_exists.py
+++ test/test_existence_detection/test_file_exists.py
@@ -4,5 +4,8 @@ from src.model.content_generation.existence_detection.file_exists import file_ex
 
 @pytest.mark.test_file_exists
 def test_file_exists():
-    pass
+    assert file_exists('test_dir/', 'hola', '.md') == True
+    assert file_exists('test_dir/', 'hi', '.md') == True
+    assert file_exists('test_dir/', 'hey', '.html') == True
+    assert file_exists('adir/', 'index', '.js') == False
 
