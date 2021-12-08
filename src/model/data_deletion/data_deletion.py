c3304dbf7d310e9993785e1398c1c3f45efedf24
Joan Nicolau <jnicolausantandreu@cifpfbmoll.eu>
2021-12-08 14:02:46 +0100

    :recycle: [data_deletion] refactorizar funcion delete_data

--- src/model/data_deletion/data_deletion.py
+++ src/model/data_deletion/data_deletion.py
@@ -1,16 +1,30 @@
+from src.model.data_extraction.values_extraction import get_values
+from pymongo.errors import OperationFailure
+
+
 def delete_data(collection):
-    documentos = collection.find({"services": {"$exists": True}})
-    servicios = []
-    for item in documentos:
-        print(item)
-        servicios.append(item["services"])
+    # get all services
+    servicios = get_values(collection, 'name')
+
+    # print all services
+    print('servicios disponibles:', servicios)
+
+    # delete document from the database
     servicio_eliminar = input("Escribe el servicio que quieres eliminar: \n")
     confirm = input("Vuelve a escribir el servicio: \n")
+
     if servicio_eliminar != confirm:
         print("Los servicios no coinciden")
+
     elif servicio_eliminar in servicios:
-        collection.delete_many({"services": servicio_eliminar})
-        print("Se ha eliminado correctamente")
+
+        try:
+            collection.delete_one({"name": servicio_eliminar})
+        except OperationFailure:
+            print("La operación ha fallado")
+        else:
+            print("Se ha eliminado correctamente")
+            return True
+
     else:
         print("item no encontrado")
-
