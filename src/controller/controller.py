8c2b5dd26597315d3cd4bd894be24ee929898da5
ErikPC <epardillo@cifpfbmoll.eu>
2021-12-09 20:30:44 +0100

    :recycle:[controller] refactorizar controller

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -4,22 +4,19 @@ from src.model.data_extraction.data_extraction import load_data
 from src.model.data_updation.update_price.update_price import update_price
 
 def controller(collection):
+    salir = False
     menu = [1,2,3,4,9]
-    while True:
+    while not salir:
         selection = input("Tiene las siguientes opciones:\n 1-Crear un servicio\n 2-Eliminar un servicio\n 3-Vicualizar un servicio\n 4-Cambiar un precio\n 9-Exit")
         if selection not in menu:
             print("No es una entrada valida")
         if selection == 1:
             create_service(collection)
-            break
         if selection == 2:
             delete_data(collection)
-            break
         if selection == 3:
             load_data(collection)
-            break
         if selection == 4:
             update_price(collection)
-            break
         if selection == 9:
-            break
+            salir = True
