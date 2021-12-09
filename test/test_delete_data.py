8dd9d1e14a068dbe88250144592dad8047bf60de
ErikPC <epardillo@cifpfbmoll.eu>
2021-12-09 18:03:16 +0100

    :white_check_mark:[test_delete_data] Añadir casos test

--- test/test_delete_data.py
+++ test/test_delete_data.py
@@ -4,16 +4,8 @@ from src.model.data_deletion import delete_data
 from src.repository.db_connection.connect_db import connect_db
 
 database = connect_db()
-collection = database['pruebas']
-# Cuenado esto funcione , hay que cambiar 'pruebas' por el nombre de de la coleccion definitiva
-collection.insert_one(
-    {
-        'nombre': 'Joan',
-        'edad': 20,
-        'deportes': ['padel', 'futbol', 'boxeo'],
-        "services": "uber"
-    }
-)
+collection = database['ufer']
+
 
 
 @pytest.mark.test_delete_data
