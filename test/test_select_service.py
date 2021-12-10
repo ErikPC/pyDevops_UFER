8dc0f3bbc57310e7cd17f8b942b4066cf708c13e
ErikPC <epardillo@cifpfbmoll.eu>
2021-12-10 17:00:44 +0100

    :coffin:[test_select_service] eliminar linea de codigo innecesaria

--- test/test_select_service.py
+++ test/test_select_service.py
@@ -1,8 +1,6 @@
 import pytest
 from src.model.service_selection.select_service.select_service import select_service
 
-services = ['Ufer Lite', 'Ufer Gold', 'Ufer Life', 'Ufer Bus']
-
 
 @pytest.mark.test_select_service
 def test_select_service():
