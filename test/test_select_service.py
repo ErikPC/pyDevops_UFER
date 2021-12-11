2afc56fcbcf20d77e4a28766e496a48f4efee2d5
ErikPC <epardillo@cifpfbmoll.eu>
2021-12-11 14:01:07 +0100

    :coffin:[test_select_service] eliminar lista innecesaria

--- test/test_select_service.py
+++ test/test_select_service.py
@@ -4,5 +4,5 @@ from src.model.service_selection.select_service.select_service import select_ser
 
 @pytest.mark.test_select_service
 def test_select_service():
-    services = ['Ufer Gold', 'Ufer Lite', 'Ufer Loco', 'Ufer Fly', 'Ufer Style']
+    services = ['Ufer Gold', 'Ufer Lite', 'Ufer Loco', 'Ufer Fly', 'Ufer Style', 'Ufer Life']
     select_service(services)
